d58c719002e6d563ff61b0a90965d9869e2e43f5
Valtteri Vuori <valtteri.vuori@live.fi>
2022-02-09 03:27:34 +0200

    Initial commit

---
+++ ToggleObjectivesList.lua
@@ -0,0 +1,8 @@
+ToggleObjectivesList = {}
+
+function ToggleObjectivesList.toggle()
+	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:Click()
+end
+
+BINDING_HEADER_TOGGLEOBJECTIVESLISTHEADER = "Toggle Objectives"
+BINDING_NAME_TOGGLEOBJECTIVESLISTNAME = "Toggle the objectives on or off"
